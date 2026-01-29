83ad604c5a4fb0d5bfda9f66f9af0fbbb97dce7d
DeaconSteiner <deaconsteiner.it@gmail.com>
2026-01-29 12:08:46 -0700

    Add docstring for can_meet Add type hints for parameters and returns Remove redundant current variable

--- Lab01/solution.py
+++ Lab01/solution.py
@@ -1,10 +1,31 @@
-def can_meet(interval):
+# solution.py
+# Deacon Steiner
+# COSC3020
+# Lab 01
+# 29 January, 2026
+
+from typing import List
+
+
+def can_meet(interval: List[List[int]]) -> bool:
+    """
+    Determines whether a person can attend all meetings without overlap.
+
+    Parameters
+    ----------
+    interval : List[List[int]]
+        A list of meeting intervals, of the form [start, end].
+
+    Returns
+    ----------
+    bool
+        True if all meetings can be attended, False otherwise.
+    """
     interval.sort(key=lambda x: x[0])
 
     prev = interval[0]
     for meeting in interval[1:]:
-        current = meeting
-        if current[0] < prev[1]:
+        if meeting[0] < prev[1]:
             return False
-        prev = current
+        prev = meeting
     return True
