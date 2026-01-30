eeb68b01b5773fb134d30b195fa81b01c7a4bc1a
DeaconSteiner <deaconsteiner.it@gmail.com>
2026-01-29 17:25:14 -0700

    Add third test case for 'touching intervals'

--- Lab01/test.py
+++ Lab01/test.py
@@ -2,6 +2,7 @@ from solution import can_meet
 
 interval1 = [[0, 30], [5, 10], [15, 20]]
 interval2 = [[7, 10], [2, 4]]
+interval3 = [[5, 7], [15, 30], [0, 5]]
 
 
 def main():
@@ -10,6 +11,10 @@ def main():
     )
     print(
         f"for the meeting times {interval2}, can you attend all meetings? {can_meet(interval2)}"
+    )
+
+    print(
+        f"for the meeting times {interval3}, can you attend all meetings? {can_meet(interval3)}"
     )
 
 
