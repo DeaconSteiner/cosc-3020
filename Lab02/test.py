6796f68fe3ad578b30803a78c71f4a51883c01dd
DeaconSteiner <deaconsteiner.it@gmail.com>
2026-02-05 10:05:27 -0700

    Add more test cases

--- Lab02/test.py
+++ Lab02/test.py
@@ -4,6 +4,9 @@
 # Lab02
 # 04 February, 2026
 
+from math import e
+from this import d
+
 from solution import tiler
 
 
@@ -11,8 +14,22 @@ def main():
     a = 168
     b = 64
 
+    c = 15
+    d = 20
+
+    e = 5
+    f = 5
+
     print(
         f"In order to tile a rectangle with length: {a} and width: {b}, you would need {tiler(a, b)[1]} tiles, with side length: {tiler(a, b)[0]}"
+    )
+
+    print(
+        f"In order to tile a rectangle with length: {c} and width: {d}, you would need {tiler(c, d)[1]} tiles, with side length: {tiler(c, d)[0]}"
+    )
+
+    print(
+        f"In order to tile a rectangle with length: {e} and width: {f}, you would need {tiler(e, f)[1]} tiles, with side length: {tiler(e, f)[0]}"
     )
 
 
