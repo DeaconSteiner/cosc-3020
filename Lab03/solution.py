54d5b50efc979a36bfc41f52129f1022a68766d7
DeaconSteiner <deaconsteiner.it@gmail.com>
2026-02-11 16:04:47 -0700

    Finish math helper functions and add test file

--- Lab03/solution.py
+++ Lab03/solution.py
@@ -4,4 +4,20 @@
 # Lab03
 # 11 February, 2026
 
+def gcd(a, b):
+    if b == 0:
+        return a;
+    return gcd(b, a % b)
+
+def ext_gcd(a, b):
+    if b == 0:
+        return (a, 1, 0)
+    (g, x1, y1) = ext_gcd(b, a % b)
+    return (g, y1, x1 - (a//b)*y1)
+
+def mod_inverse(e, phi):
+    g, x, y = ext_gcd(e, phi)
+    if g != 1:
+        raise ValueError("No inverse exists")
+    return x % phi
 
---
+++ Lab03/test.py
@@ -0,0 +1,13 @@
+# test.py
+# Deacon Steiner
+# COSC 3020
+# Lab03
+# 11 February, 2026
+
+from solution import ext_gcd, gcd
+
+def main():
+    print(gcd(30, 12))
+    print(ext_gcd(30, 12))
+if __name__ == "__main__":
+    main()
