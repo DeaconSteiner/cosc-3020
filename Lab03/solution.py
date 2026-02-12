59d5af590a5cbb85e0044f9fb21c7e2d764b2771
DeaconSteiner <deaconsteiner.it@gmail.com>
2026-02-11 20:51:27 -0700

    Complete assignment

--- Lab03/solution.py
+++ Lab03/solution.py
@@ -21,3 +21,45 @@ def mod_inverse(e, phi):
         raise ValueError("No inverse exists")
     return x % phi
 
+def rsa_keygen(p, q, e):
+    n = p*q
+    phi = (p-1)*(q-1)
+    if gcd(e, phi) == 1:
+        d = mod_inverse(e, phi)
+        public_key = (e, n)
+        private_key = (d, n)
+    else:
+        raise ValueError("E and phi are not coprime")
+    return public_key, private_key
+
+def encrypt(m, public_key):
+    e, n = public_key
+    return pow(m, e, n)
+
+def decrypt(c, private_key):
+    d, n = private_key  
+    return pow(c, d, n)
+
+def main():
+    p = 61
+    q = 53
+    e = 17
+    n = p*q
+    phi = (p-1)*(q-1)
+
+    pub, priv = rsa_keygen(p, q, e)
+    print(f"N = {n}, phi = {phi}")
+    print(f"Public key is: {pub} and Private key is: {priv}")
+
+    for grade in [92, 7, 100]:
+        ciphertext = encrypt(grade, pub)
+        print(f"Encrypted grade is: {ciphertext}")
+        recovered = decrypt(ciphertext, priv)
+        print(f"Recovered grade is: {recovered}")
+        if recovered == grade:
+            print("OK\n")
+        else: 
+            raise ValueError(f"Mismatch: {recovered} != {grade}")
+
+if __name__ == "__main__":
+    main()
--- Lab03/test.py
+++ Lab03/test.py
@@ -4,10 +4,15 @@
 # Lab03
 # 11 February, 2026
 
-from solution import ext_gcd, gcd
+from solution import *
 
 def main():
     print(gcd(30, 12))
     print(ext_gcd(30, 12))
+    print(mod_inverse(17, 3120))
+    pub, priv = rsa_keygen(61, 53, 17)
+    print(pub)
+    print(priv)
+
 if __name__ == "__main__":
     main()
