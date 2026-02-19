421a7283091ff52cef708704dd86dcedd20875bc
DeaconSteiner <deaconsteiner.it@gmail.com>
2026-02-18 18:00:46 -0700

    Finish divide-and-conquer algorithm and add additional test case

--- Lab04/solution.py
+++ Lab04/solution.py
@@ -30,3 +30,19 @@ def binary_find(matrix: List[List[int]], target: int) -> bool:
             return True
     return False
 
+# Divide-and-conquer algorithm
+def dac_find(matrix: List[List[int]], target: int) -> bool:
+    if not matrix or not matrix[0]:
+        return False
+
+    r, c = 0, len(matrix[0])-1
+    
+    while r < len(matrix) and c >= 0:
+        if target == matrix[r][c]:
+            return True
+        elif target < matrix[r][c]:
+            c -= 1
+        else:
+            r += 1
+
+    return False
--- Lab04/test.py
+++ Lab04/test.py
@@ -21,6 +21,20 @@ def main():
 
     dac_result = dac_find(matrix, target)
     print(f"Divide-and-Conquer says: {dac_result}")
+    
+    # Test 2 - Should return false
+    matrix = [[1,4,7,11,15],
+              [2,5,8,12,19],
+              [3,6,9,16,22],
+              [10,13,14,17,24],
+              [18,21,23,26,30]]
+    target = 20
+
+    binary_result = binary_find(matrix, target)
+    print(f"Binary Search says: {binary_result}")
+
+    dac_result = dac_find(matrix, target)
+    print(f"Divide-and-Conquer says: {dac_result}")
 
 if __name__ == "__main__":
     main()
